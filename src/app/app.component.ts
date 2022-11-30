3e4df1932999fe67cddb154883b01ab0dc434d66
muthu973 <muthuprabhakar2000@gmail.com>
2022-11-30 12:42:19 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'multilanguage';
+
+    constructor(public translate:TranslateService){
+
+      translate.addLangs(['en','ta']);
+      translate.setDefaultLang('en');
+    }
+
+    switchLang(lang:string){
+      console.log(lang);
+      this.translate.use(lang);
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateModule,TranslateLoader } from '@ngx-translate/core';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function httpTranslate(http:HttpClient){
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide:TranslateLoader,
+        useFactory:httpTranslate,
+        deps:[HttpClient]
+      }
+    }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
